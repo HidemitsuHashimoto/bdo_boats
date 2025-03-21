4539111cdb36e3e0dd3b7d0ca9d8dddfebb66676
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2025-03-20 23:01:16 -0300

    feat: change item name

---
+++ lib/providers/material_list/carraca_epheria_bravura_proa_shiro.dart
@@ -0,0 +1,7 @@
+import 'package:bdo_boats/domain/material.dart';
+
+Material carracaEpheriaBravuraProaShiro = Material(
+  id: 'carracaEpheriaBravuraProaShiro',
+  name: 'Carraca de Epheria Bravura: Proa de Shiro',
+  quantity: 0,
+);
---
+++ lib/providers/material_list/carraca_epheria_equilibrio_casco_negro_shiro.dart
@@ -0,0 +1,7 @@
+import 'package:bdo_boats/domain/material.dart';
+
+Material carracaEpheriaEquilibrioCascoNegroShiro = Material(
+  id: 'carracaEpheriaEquilibrioCascoNegroShiro',
+  name: 'Carraca de Epheria Equilíbrio: Casco Negro de Shiro',
+  quantity: 0,
+);
