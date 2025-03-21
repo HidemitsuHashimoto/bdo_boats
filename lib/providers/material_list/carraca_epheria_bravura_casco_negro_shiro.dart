54aba721d6e84f0da239d9d909f6a3d7ad68a519
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2025-03-20 22:59:43 -0300

    feat: change item name

---
+++ lib/providers/material_list/carraca_epheria_bravura_casco_negro_shiro.dart
@@ -0,0 +1,7 @@
+import 'package:bdo_boats/domain/material.dart';
+
+Material carracaEpheriaBravuraCascoNegroShiro = Material(
+  id: 'carracaEpheriaBravuraCascoNegroShiro',
+  name: 'Carraca de Epheria Bravura: Casco Negro de Shiro',
+  quantity: 0,
+);
